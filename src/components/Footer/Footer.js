7a726050bd5c69e6c1a7a4f50bf2ebdb69c4af95
RyaN_AwAjI <102438965+RyaNAwAji1@users.noreply.github.com>
2022-06-26 01:13:39 +0300

    React

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+     <span style={{
+      color:"Blue",
+      fontSize:"20px",
+      margin:"0 5px"
+      
+     }}>RyanAwaji</span>
+      <a
+        href="https://www.youtube.com/roadsidecoder"
+        style={{ cursor: "pointer" }}
+      >
+        Tuwaiq Academy
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
